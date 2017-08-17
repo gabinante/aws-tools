19878fb0afeaa2a60aa00b58d6073db8989dcf9a
Gabe Abinante <gabinante@clearslide.com>
2017-08-17 12:11:03 -0700

    add keyloop

---
+++ ec2/keyloop.sh
@@ -0,0 +1,19 @@
+ keyloop() {
+      me=`whoami`
+      sshdir="/Users/$me/.ssh"
+      users="centos ec2-user root ubuntu"
+      host=$1
+      [ "$#" -ne 1 -a "$#" ] && echo "Usage: keyloop <ipaddress>" && return 100
+     echo "Trying all the keys, this might take a minute..."
+     for dir in $sshdir
+      do
+        find $sshdir -type f -name "*.pem"
+      done > /tmp/keylist.txt
+      for line in `cat /tmp/keylist.txt`;
+        do
+          for user in $users
+            do
+              ssh -ttX -i $line $user@$1 2> /dev/null
+          done
+        done
+  }
